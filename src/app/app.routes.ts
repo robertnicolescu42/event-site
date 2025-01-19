47faafd2ccce201987ef25bab1ebb9ab8962fac2
Robert Nicolescu <robertnicolescu42@yahoo.com>
2025-01-19 17:42:31 +0200

    Fixed routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './events/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home/:city',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'home/pitesti',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'home/pitesti',
+  },
+];
--- src/app/events/home/home.component.ts
+++ src/app/events/home/home.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventService, Event } from '../../core/event.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  city: string = '';
+  event: Event | undefined;
 
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      console.log(
+        '🚀 ~ HomeComponent ~ this.route.paramMap.subscribe ~ params:',
+        params
+      );
+      this.city = params.get('city')!;
+      console.log('City:', this.city);
+    });
+  }
 }
