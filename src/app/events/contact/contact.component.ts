d5ce7a56e81e8596d6616f742905699fe08fe58a
Robert Nicolescu <robertnicolescu42@yahoo.com>
2025-01-20 14:19:46 +0200

    Fixed form, added other fields

--- src/app/events/contact/contact.component.ts
+++ src/app/events/contact/contact.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {
   FormBuilder,
@@ -9,15 +10,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-contact',
   imports: [
+    MatCheckboxModule,
     MatCardModule,
     MatInputModule,
     MatFormFieldModule,
     ReactiveFormsModule,
     MatButtonModule,
+    CommonModule,
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
@@ -27,10 +31,19 @@ export class ContactComponent {
 
   constructor(private fb: FormBuilder) {
     this.contactForm = this.fb.group({
-      name: ['', Validators.required],
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.pattern('^[a-zA-Z ]*$'),
+        ],
+      ],
       email: ['', [Validators.required, Validators.email]],
       phone: ['', Validators.pattern('^[0-9]{10}$')],
-      message: ['', Validators.required],
+      message: [''],
+      gdpr: [false, Validators.requiredTrue],
+      newsletter: [false],
     });
   }
 
