7f1bc04e0784b88d6fd090a4f1dc2439962a0cd7
Robert Nicolescu <robertnicolescu42@yahoo.com>
2025-01-26 19:52:30 +0200

    added social link

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatIconModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
