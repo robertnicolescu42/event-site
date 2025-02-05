b705985887f5aea18353f792d98398d8e061b4ed
Robert Nicolescu <robertnicolescu42@yahoo.com>
2025-02-05 21:20:33 +0200

    refactor styles and markup for improved layout and aesthetics; remove unused analytics code

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+// import { getAnalytics } from 'firebase/analytics';
 import { firebaseConfig } from './environments/environment';
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
@@ -10,4 +10,4 @@ bootstrapApplication(AppComponent, appConfig).catch((err) =>
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
