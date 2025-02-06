0adae47ec2aaa2ae45432a74856aff038bd617fe
Robert Nicolescu <robertnicolescu42@yahoo.com>
2025-02-06 16:21:38 +0200

    add under construction component and update app layout; modify styles and implement basic tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./core/header/header.component";
-import { FooterComponent } from "./core/footer/footer.component";
-
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { underConstruction } from '../environments/environment';
+import { UnderConstructionComponent } from './core/under-construction/under-construction.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    UnderConstructionComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'event-site';
+
+  isUnderConstruction: boolean = true;
 }
---
+++ src/app/core/under-construction/under-construction.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-under-construction',
+  imports: [MatCardModule, MatIconModule],
+  templateUrl: './under-construction.component.html',
+  styleUrl: './under-construction.component.scss',
+})
+export class UnderConstructionComponent {}
